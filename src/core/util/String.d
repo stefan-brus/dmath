ef6a0fadc8867c5007554786c166ed0928071158
Stefan Brus <stefan1brus@gmail.com>
2014-03-05 00:55:32 +0100

    Add unittests to String
    Addresses #3.
    Adds unittests to the String module.

--- src/core/util/String.d
+++ src/core/util/String.d
@@ -13,7 +13,7 @@ private import std.ascii;
 
 /**
  * Checks if a given character is valid
- * NOTE: Currently follows the lexical rules of DMath
+ * NOTE: Currently follows the lexical rules of DMath for string tokens
  *
  * Params:
  *      c = The character to validate
@@ -25,4 +25,28 @@ private import std.ascii;
 public bool isValidChar ( char c )
 {
     return isAlpha(c);
+}
+
+
+/**
+ * Unittests
+ */
+
+unittest
+{
+    /**
+     * Error message
+     */
+
+    const err_msg = "String unittests failed";
+
+
+    /**
+     * isValidChar
+     */
+
+    assert(isValidChar('a'), err_msg);
+    assert(isValidChar('Q'), err_msg);
+    assert(!isValidChar('2'), err_msg);
+    assert(!isValidChar('+'), err_msg);
 }
